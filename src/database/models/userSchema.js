b349605884f2cdf3b905f0b0155357e1b5d53342
Davidson Ratis <davidsonmakarios@gmail.com>
2020-08-02 23:29:32 -0300

    custom validations

--- src/database/models/userSchema.js
+++ src/database/models/userSchema.js
@@ -3,17 +3,35 @@ const mongoose = require('../index')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        validate: {
+            validator(v){
+              return /^[a-z][a-z\s]*$/i.test(v)
+            },
+            message: props => `${props.value} is not a valid name!`
+        },
+        required: [true, "Please enter name."]
     },
     fone: {
         type: String,
-        required: true
+        validate: {
+            validator(v){
+              return /^[0-9]*$/.test(v)
+            },
+            message: props => `${props.value} is not a valid fone!`
+        },
+        required: [true, "Please enter fone."]
     },
     email: {
         type: String,
-        unique: true,
+        validate: {
+            validator(v){
+              return /\S+@\S+\.\S+/.test(v)
+            },
+            message: props => `${props.value} is not a valid email!`
+        },
+        unique: [true, "Email must be unique."],
         lowercase: true,
-        required: true,
+        required: [true, "Please enter email."]
     }
 })
 
