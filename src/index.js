609d8ba38059bb1357d8c2fc4ed3ff9ea73a0246
Davidson Ratis <davidsonmakarios@gmail.com>
2020-08-04 14:22:00 -0300

    treating as server exceptions

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const port = 3000
 middlewares(app)
 routes(app)
 
+//exceptions and server configuration
+app.use((req, res, next) =>{
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) =>{
+    const {status, message } = error
+    res.status(status || 500)
+    res.json({ error: message })
+})
+
 app.listen(port, () => {
     console.log(`Server running on port http://localhost${port}`)
 })
