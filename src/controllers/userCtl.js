5fbfbea2746cc87f0ebc9ad04089bd882d54e54a
Davidson Ratis <davidsonmakarios@gmail.com>
2020-08-04 14:24:15 -0300

    adding route paths

--- src/controllers/userCtl.js
+++ src/controllers/userCtl.js
@@ -1,5 +1,61 @@
+const users = require('../database/models/userSchema')
+
 module.exports = {
-    getAll(req, res) {
-        return res.send('test get')
+    async findAll(req, res, next) {
+        try {
+            const page = parseInt(req.query.page) || 1
+            const limit = 5
+            
+            const data = await users.find().limit(limit)
+                .skip((page - 1) * limit).exec()
+            
+            const count = await users.countDocuments()
+
+            res.header('X-Total-Count', count)
+            return res.json({
+                data,
+                count,
+                limit,
+                page
+            })
+        } catch (error) {
+            next(error)
+        }
+    },
+    async create(req, res, next) {
+        const user = { ...req.body }
+
+        if (await users.findOne({ email: user.email }))
+            return res.status(400).json({ error: 'User already exists' })
+
+        try {
+            await users.create(user)
+            return res.status(201).send()
+        } catch (error) {
+            next(error)
+        }
+    },
+    async update(req, res, next) {
+        const user = { ...req.body }
+        const userId = req.params.id
+
+        try {
+            await users.findByIdAndUpdate(userId, user)
+            return res.send()
+        } catch(error) {
+            next(error)
+        }
+    },
+    async remove(req, res, next) {
+        const userId = req.params.id
+
+        try {
+            const delrow = await users.findByIdAndRemove(userId)
+            if (!delrow) throw Error('User not found')
+            return res.send()
+        } catch(error) {
+            error.status = 400
+            next(error)
+        }
     }
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,10 @@
 const controllers = require('../controllers/userCtl')
 
 module.exports = app => {
-    app.route('/')
-        .get(controllers.getAll)
+    app.route('/users')
+        .get(controllers.findAll)
+        .post(controllers.create)
+    app.route('/users/:id')
+        .put(controllers.update)
+        .delete(controllers.remove)
 }
