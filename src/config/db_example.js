a91bd240e03da0fb741f587aa12a72af28bda453
Davidson Ratis <davidsonmakarios@gmail.com>
2020-08-19 17:48:02 -0300

    Example of a Configuration File for the Database

---
+++ src/config/db_example.js
@@ -0,0 +1,11 @@
+/* Change the file name to db, so that it is used by mogoose in: 
+   database/index.js */
+const config = {
+    host: 'myHost',
+    user: 'myUser',
+    pass: 'myPssword',
+    port: 'myPort',
+    db: 'myDatabase'
+}
+
+module.exports = config
