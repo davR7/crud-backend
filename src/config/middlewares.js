6b13753663f0868636f5c53a5ae214baf6c9a006
Davidson Ratis <davidsonmakarios@gmail.com>
2020-08-19 23:00:53 -0300

    changing the cors configuration

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,11 +1,10 @@
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
 const cors = require('cors')
-const frontendUrl = 'http://localhost:8080'
 
 module.exports = app => {
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: false }))
     app.use(helmet())
-    app.use(cors({ "origin": frontendUrl }))
+    app.use(cors())
 }
