eb4fb7c691ac0b622386821e4df0b7e5ec9c926e
Davidson Ratis <58185554+davR7@users.noreply.github.com>
2020-11-05 12:01:58 -0300

    minor corrections and addition of status 422.

--- src/controllers/userCtl.js
+++ src/controllers/userCtl.js
@@ -1,4 +1,5 @@
-const users = require('../database/models/userSchema')
+const mongoose = require('mongoose')
+const userModel = require('../database/models/userSchema')
 
 module.exports = {
     async findAll(req, res, next) {
@@ -6,10 +7,10 @@ module.exports = {
             const page = parseInt(req.query.page) || 1
             const limit = parseInt(req.query.limit) || 0
             
-            const data = await users.find().limit(limit)
-                .skip((page - 1) * limit).exec()
+            const data = await userModel.find().limit(limit)
+                .skip((page - 1) * limit)
             
-            const count = await users.countDocuments()
+            const count = await userModel.countDocuments()
 
             res.header('X-Total-Count', count)
             return res.json({
@@ -25,11 +26,11 @@ module.exports = {
     async create(req, res, next) {
         const user = { ...req.body }
 
-        if (await users.findOne({ email: user.email }))
+        if (await userModel.findOne({ email: user.email }))
             return res.status(400).json({ error: 'User already exists' })
 
         try {
-            await users.create(user)
+            await userModel.create(user)
             return res.status(201).send()
         } catch (error) {
             next(error)
@@ -40,10 +41,14 @@ module.exports = {
         const userId = req.params.id
 
         try {
-            const dbUser = await users.findByIdAndUpdate(userId, user)
-            if (!dbUser){
+            const idIsValid = mongoose.Types.ObjectId.isValid(userId)
+            if (!idIsValid) 
+                return res.status(422).json({ error: 'Invalid id' })
+            
+            const dbUser = await userModel.findByIdAndUpdate(userId, user)
+            if (!dbUser)
                 return res.status(204).json()
-            }
+                
             return res.json()
         } catch(error) {
             next(error)
@@ -53,10 +58,14 @@ module.exports = {
         const userId = req.params.id
 
         try {
-            const dbUser = await users.findByIdAndRemove(userId)
-            if (!dbUser){
+            const idIsValid = mongoose.Types.ObjectId.isValid(userId)
+            if (!idIsValid) 
+                return res.status(422).json({ error: 'Invalid id' })
+            
+            const dbUser = await userModel.findByIdAndRemove(userId)
+            if (!dbUser)
                 return res.status(204).json()
-            }
+            
             return res.json()
         } catch(error) {
             next(error)
